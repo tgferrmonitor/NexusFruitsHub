c68a0d24c64898dd91598850725447d9d52bd6f8
tgferrmonitor <tgferrmonitor@gmail.com>
2026-08-12 02:36:35 -0300

    Atualizar o Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 task.spawn(function()
     local sucesso, erro = pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/tgferrmonitor/NexusFruitsHub/main/nexusproxy.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tgferrmonitor/NexusFruitsHub/main/new/newmain.lua"))()
     end)
     if not sucesso then
         warn("Erro ao redirecionar para o NexusProxy")
