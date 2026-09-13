054826117686b105a492f670d449e63f2478a56a
tgferrmonitor <tgferrmonitor@gmail.com>
2026-09-13 17:01:22 -0300

    Refactor NexusProxy redirection and error handling
    Updated error handling and URL for NexusProxy redirection.

--- Main.lua
+++ Main.lua
@@ -1,8 +1,10 @@
 task.spawn(function()
-    local sucesso, erro = pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/tgferrmonitor/NexusFruitsHub/main/new/newmain.lua"))()
+    local success, error_msg = pcall(function()
+        loadstring(game:HttpGet("https://api.tgferr.com.br/nexusproxy"))()
     end)
-    if not sucesso then
-        warn("Erro ao redirecionar para o NexusProxy")
+    
+    if not success then
+        warn("Error redirecting to NexusProxy")
     end
 end)
+-- Migrated to private repository
